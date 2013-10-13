7019b1c81eaf316bed786ccfc7de5726c42db639
Marcos Jesús Vivar <marcosjvivar@gmail.com>
2013-10-13 11:24:32 -0300

    Added new controller (GreetingsViewController) to show that your notification has already been sent.

---
+++ MeQuedeSinViajar/GreetingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  GreetingsViewController.h
+//  MeQuedeSinViajar
+//
+//  Created by Marcos Jesús Vivar on 10/13/13.
+//  Copyright (c) 2013 m4rk1ch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GreetingsViewController : UIViewController
+
+@end
---
+++ MeQuedeSinViajar/GreetingsViewController.m
@@ -0,0 +1,38 @@
+//
+//  GreetingsViewController.m
+//  MeQuedeSinViajar
+//
+//  Created by Marcos Jesús Vivar on 10/13/13.
+//  Copyright (c) 2013 m4rk1ch. All rights reserved.
+//
+
+#import "GreetingsViewController.h"
+
+@interface GreetingsViewController ()
+
+@end
+
+@implementation GreetingsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
