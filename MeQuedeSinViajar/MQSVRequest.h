69b8109a871713cbd59b3018fe9bc1e690d740a9
Marcos Jesús Vivar <marcosjvivar@gmail.com>
2013-10-13 01:51:39 -0300

    Added request constructor so as to have a communication service layer in MQSVRequest class.

---
+++ MeQuedeSinViajar/MQSVRequest.h
@@ -0,0 +1,47 @@
+//
+//  MQSVRequest.h
+//  MeQuedeSinViajar
+//
+//  Created by Marcos Jesús Vivar on 10/12/13.
+//  Copyright (c) 2013 m4rk1ch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "SBJson.h"
+#import "MQSVAppConfiguration.h"
+
+@class MQSVRequest;
+
+typedef enum
+{
+    HTTPStatusCodeOK = 200,
+    HTTPStatusCodeCreated = 201,
+    HTTPStatusCodeNoContent = 204,
+    HTTPStatusCodeUnprocessableEntity = 422,
+    HTTPStatusCodeServerError = 500
+} HTTPStatusCode;
+
+typedef void (^MQSVRequestCompletionBlock)(MQSVRequest *request, NSInteger httpStatusCode, id parsedJSONResponse);
+
+@interface MQSVRequest : NSObject
+
+@property (nonatomic, readonly, copy) NSString *path;
+@property (nonatomic, readonly, copy) NSString *httpMethod;
+@property (nonatomic, readonly, copy) NSString *requestType;
+
+@property (nonatomic, readonly, copy) NSHTTPURLResponse *httpURLResponse;
+@property (nonatomic, readonly) NSInteger statusCode;
+
++ (id)performRequestWithPath:(NSString *)path method:(NSString *)httpMethod completionBlock:(MQSVRequestCompletionBlock)completion;
++ (id)requestWithPath:(NSString *)path method:(NSString *)httpMethod;
++ (id)requestWithPath:(NSString *)path method:(NSString *)httpMethod requestType:(NSString *)type;
+- (id)initWithPath:(NSString *)path method:(NSString *)httpMethod;
+- (id)initWithPath:(NSString *)path method:(NSString *)httpMethod requestType:(NSString *)type;
+
+- (void)startWithCompletionBlock:(MQSVRequestCompletionBlock)completionBlock;
+- (void)stop;
+- (void)setFormParameters:(NSDictionary *)params;
+- (BOOL)statusCodeIndicatesSuccess;
+
+@end
---
+++ MeQuedeSinViajar/MQSVRequest.m
@@ -0,0 +1,248 @@
+//
+//  MQSVRequest.m
+//  MeQuedeSinViajar
+//
+//  Created by Marcos Jesús Vivar on 10/12/13.
+//  Copyright (c) 2013 m4rk1ch. All rights reserved.
+//
+
+#import "MQSVRequest.h"
+
+@interface MQSVRequest ()
+
+@property (nonatomic, strong) NSMutableData *downloadedData;
+@property (nonatomic, copy) MQSVRequestCompletionBlock completionBlock;
+@property (nonatomic, readwrite, copy) NSString *path;
+@property (nonatomic, readwrite, copy) NSString *httpMethod;
+@property (nonatomic, readwrite, copy) NSString *requestType;
+@property (nonatomic, strong) NSURLConnection *connection;
+@property (nonatomic, readwrite, copy) NSHTTPURLResponse *httpURLResponse;
+@property (nonatomic, copy) NSString *formParameterString;
+
+- (NSURL *)requestURLWithPath:(NSString *)path;
+- (void)performCompletionBlockOnMainThreadWithJSON:(id)jsonObject;
+
+@end
+
+@implementation MQSVRequest
+
+@synthesize path = _path;
+@synthesize httpMethod = _httpMethod;
+@synthesize requestType = _requestType;
+@synthesize downloadedData = _downloadedData;
+@synthesize completionBlock = _completionBlock;
+@synthesize connection = _connection;
+@synthesize httpURLResponse = _httpURLResponse;
+@synthesize formParameterString = _formParameterString;
+
+#pragma mark - Instantiation
+
++ (id)requestWithPath:(NSString *)path method:(NSString *)httpMethod
+{
+    return [[self alloc] initWithPath:path method:httpMethod];
+}
+
++ (id)requestWithPath:(NSString *)path method:(NSString *)httpMethod requestType:(NSString *)type
+{
+    return [[self alloc] initWithPath:path method:httpMethod requestType:type];
+}
+
+- (id)initWithPath:(NSString *)path method:(NSString *)method requestType:(NSString *)type
+{
+    self = [super init];
+    if (!self)
+        return nil;
+    
+    _path = [path copy];
+    _httpMethod = [method copy];
+    _requestType = [type copy];
+    
+    return self;
+}
+
+- (id)initWithPath:(NSString *)path method:(NSString *)method
+{
+    self = [super init];
+    if (!self)
+        return nil;
+    
+    _path = [path copy];
+    _httpMethod = [method copy];
+    _requestType = @"1";
+    
+    return self;
+}
+
++ (id)performRequestWithPath:(NSString *)path method:(NSString *)httpMethod completionBlock:(MQSVRequestCompletionBlock)completion
+{
+    MQSVRequest *request = [self requestWithPath:path method:httpMethod];
+    [request startWithCompletionBlock:completion];
+    
+    return request;
+}
+
+#pragma mark - Completion Block
+
+- (void)startWithCompletionBlock:(MQSVRequestCompletionBlock)completionBlock
+{
+    if (self.connection)
+    {
+        [self stop];
+    }
+    
+    self.completionBlock = completionBlock;
+    
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[self requestURLWithPath:self.path]];
+    [request setHTTPMethod:self.httpMethod];
+	[request setValue:@"application/json" forHTTPHeaderField:@"Accept"];
+	[request addValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+    
+    BOOL shouldEmbedParameters = [self.formParameterString length] > 0 && ([self.httpMethod isEqualToString:@"POST"] || [self.httpMethod isEqualToString:@"PUT"]);
+    if (shouldEmbedParameters)
+    {
+        [request setHTTPBody:[self.formParameterString dataUsingEncoding:NSUTF8StringEncoding]];
+    }
+    
+    self.connection = [NSURLConnection connectionWithRequest:request delegate:self];
+}
+
+- (void)stop
+{
+    [self.connection cancel];
+    self.connection = nil;
+    
+    self.downloadedData = nil;
+    self.completionBlock = nil;
+}
+
+#pragma mark - NSURLConnectionDelegate
+
+- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response
+{
+    if ([response isKindOfClass:[NSHTTPURLResponse class]])
+    {
+        NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *)response;
+        
+        self.httpURLResponse = httpResponse;
+        
+        NSDictionary *header = [httpResponse allHeaderFields];
+        NSString *contentLength = [header valueForKey:@"Content-Length"];
+        self.downloadedData = [NSMutableData dataWithCapacity:[contentLength integerValue]];
+    }
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data
+{
+    [self.downloadedData appendData:data];
+}
+
+- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error
+{
+    NSLog(@"Web service request failed, Path: %@", self.path);
+    
+    self.completionBlock(self, NO, nil);
+    
+    self.connection = nil;
+}
+
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection
+{
+    BOOL dataDidDownload = ([self.downloadedData length] > 0);
+    BOOL noContent = (self.statusCode == HTTPStatusCodeNoContent);
+    
+    if (dataDidDownload || noContent)
+    {
+        dispatch_queue_t globalConcurrentQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+        dispatch_async(globalConcurrentQueue, ^{
+            NSString *response = [[NSString alloc] initWithData:self.downloadedData encoding:NSUTF8StringEncoding];
+            
+            SBJsonParser *jsonParser = [[SBJsonParser alloc] init];
+            id parsedJSON = [jsonParser objectWithString:response];
+            
+            [self performCompletionBlockOnMainThreadWithJSON:parsedJSON];
+        });
+    }
+    
+    self.connection = nil;
+}
+
+- (void)performCompletionBlockOnMainThreadWithJSON:(id)jsonObject
+{
+    if (!self.completionBlock)
+        return;
+    
+    dispatch_async(dispatch_get_main_queue(), ^{
+        self.completionBlock(self, self.statusCode, jsonObject);
+    });
+}
+
+#pragma mark - URL Constructor
+
+- (NSURL *)requestURLWithPath:(NSString *)path
+{
+    if ([path hasPrefix:@"http"])
+    {
+        return [NSURL URLWithString:path];
+    }
+    
+    NSString *baseURL = MQSV_BASE_API_URL;
+    
+    NSString *urlString = nil;
+    BOOL shouldAppendFormParameters = (([self.formParameterString length] > 0) && [self.httpMethod isEqualToString:@"GET"]);
+    if (shouldAppendFormParameters)
+    {
+        urlString = baseURL;
+    }
+    else
+    {
+        urlString = baseURL;
+    }
+    
+    NSLog(@"URL Request: %@",urlString);
+    
+    return [NSURL URLWithString:urlString];
+}
+
+- (NSInteger)statusCode
+{
+    return [self.httpURLResponse statusCode];
+}
+
+- (BOOL)statusCodeIndicatesSuccess
+{
+    switch (self.statusCode)
+    {
+        case HTTPStatusCodeOK:
+        case HTTPStatusCodeCreated:
+        case HTTPStatusCodeNoContent:
+            return YES;
+    }
+    
+    return NO;
+}
+
+- (void)setFormParameters:(NSDictionary *)params
+{
+    NSStringEncoding encoding = NSUTF8StringEncoding;
+    
+    NSMutableString *parameterString = [NSMutableString string];
+    
+    for (NSString *paramKey in params)
+    {
+        NSString *value = [params objectForKey:paramKey];
+        
+        CFStringEncoding convertedEncoding = CFStringConvertNSStringEncodingToEncoding(encoding);
+        NSString *addString = @"&=+";
+        NSString *escapedString = (NSString *)CFBridgingRelease(CFURLCreateStringByAddingPercentEscapes(kCFAllocatorDefault, (CFStringRef)value, (CFStringRef)nil, (CFStringRef)addString, convertedEncoding));
+        
+        NSString *encodedValue = escapedString;
+        
+        NSUInteger length = [parameterString length];
+        NSString *paramFormat = (length == 0 ? @"%@=%@" : @"&%@=%@");
+        [parameterString appendFormat:paramFormat, encodedValue];
+    }
+    
+    self.formParameterString = parameterString;
+}
+
+@end
