c3f92d49d2b443038362884fcd71cecdbc1cfaa7
Marcos Jesús Vivar <marcosjvivar@gmail.com>
2013-10-13 01:50:04 -0300

    Added configuration app keys.

---
+++ MeQuedeSinViajar/MQSVAppConfiguration.h
@@ -0,0 +1,15 @@
+//
+//  MQSVAppConfiguration.h
+//  MeQuedeSinViajar
+//
+//  Created by Marcos Jesús Vivar on 10/12/13.
+//  Copyright (c) 2013 m4rk1ch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#define MQSV_BASE_API_URL @"me-quede-sin-viajar.herokuapp.com/"
+
+#define HTTP_GET_METHOD @"GET"
+#define HTTP_POST_METHOD @"POST"
+#define HTTP_PUT_METHOD @"PUT"
