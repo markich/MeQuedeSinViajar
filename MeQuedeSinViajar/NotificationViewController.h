480ce128261cf909656afe3e8d65a1fcfb4c177f
Marcos Jesús Vivar <marcosjvivar@gmail.com>
2013-10-13 01:54:04 -0300

    Added notification controller in order to show greeting message after notifying server.

---
+++ MeQuedeSinViajar/NotificationViewController.h
@@ -0,0 +1,13 @@
+//
+//  NotificationViewController.h
+//  MeQuedeSinViajar
+//
+//  Created by Marcos Jesús Vivar on 10/12/13.
+//  Copyright (c) 2013 m4rk1ch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NotificationViewController : UIViewController
+
+@end
---
+++ MeQuedeSinViajar/NotificationViewController.m
@@ -0,0 +1,38 @@
+//
+//  NotificationViewController.m
+//  MeQuedeSinViajar
+//
+//  Created by Marcos Jesús Vivar on 10/12/13.
+//  Copyright (c) 2013 m4rk1ch. All rights reserved.
+//
+
+#import "NotificationViewController.h"
+
+@interface NotificationViewController ()
+
+@end
+
+@implementation NotificationViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
